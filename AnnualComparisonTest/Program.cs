4e7f968ead24f4ec15548c137979ad9ce9a1c7e5
Keenan Carroll <keenan.carroll.va@gmail.com>
2018-08-16 15:41:56 -0400

    Created simple annual comparrison

--- AnnualComparisonTest/Program.cs
+++ AnnualComparisonTest/Program.cs
@@ -10,6 +10,31 @@ namespace AnnualComparisonTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1");
+            Console.WriteLine("What is your hourly rate?");
+            int hourlyRate = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many hours do you work in a week?");
+            int hourWeekly = Convert.ToInt32(Console.ReadLine());
+            int totalOne = hourlyRate * hourWeekly * 52;
+
+            Console.WriteLine("Person 2");
+            Console.WriteLine("What is your hourly rate?");
+            int hourlyRateTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many hours do you work in a week?");
+            int hourWeeklyTwo = Convert.ToInt32(Console.ReadLine());
+            int totalTwo = hourlyRateTwo * hourWeeklyTwo * 52;
+
+            Console.WriteLine("Annual salary of Person 1:");
+            Console.WriteLine(totalOne);
+
+            Console.WriteLine("Annual salary of Person 2:");
+            Console.WriteLine(totalTwo);
+
+            bool compareOneTwo = totalOne > totalTwo;
+            Console.WriteLine("Does Person 1 make more than Person 2?");
+            Console.WriteLine(compareOneTwo);
+            Console.Read();
         }
     }
 }
